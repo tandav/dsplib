41a584d7093dfba620053de13402bba5ce72a568
Alexander Rodionov <tandav@tandav.me>
2022-10-04 22:57:50 +0300

    fix type annotation

--- dsptool/scale.py
+++ dsptool/scale.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def minmax_scaler(value: float | np.ndarray, oldmin, oldmax, newmin=0.0, newmax=1.0) -> float:
+def minmax_scaler(value: float | np.ndarray, oldmin, oldmax, newmin=0.0, newmax=1.0) -> float | np.ndarray:
     if not oldmin < oldmax:
         raise ValueError('oldmin should be less than oldmax')
     if not newmin < newmax:
